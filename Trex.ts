0dadbd47651aea036f89f7224bb57055c27da9fa
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-06-05 20:59:18 -0400

    refact files

--- Trex.ts
+++ Trex.ts
@@ -1,47 +1,51 @@
-import {
-  green,
-  yellow,
-  white,
-  red,
-  cyan,
-} from "https://deno.land/std/fmt/colors.ts";
+import { green, yellow, white, red, cyan } from "https://deno.land/std/fmt/colors.ts";
 import { installPakages, updatePackages } from "./handlers/handle_packages.ts";
 import { STD, VERSION, helpsInfo, flags, keyWords } from "./utils/info.ts";
 import { checkPackage, createPackage } from "./handlers/handle_files.ts";
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import { LogHelp, Version, updateTrex } from "./utils/logs.ts";
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import exec from "./tools/install_tools.ts";
 import dbTool from "./tools/database.ts";
 
 const input = Deno.args;
 
 if (input[0] === keyWords.install || input[0] === keyWords.i) {
+
   if (existsSync("./import_map.json")) {
     const data = JSON.parse(checkPackage());
-    const oldPackage = updatePackages(data);
+    const oldPackage = updatePackages(data) as { error?: string,  were?: string };
     const newPAckage = installPakages(input);
-    // @ts-ignore
+
     if (oldPackage?.error) {
-      // @ts-ignore
       console.error(yellow(`in: ${white(`${oldPackage.were}`)}`));
-      //@ts-ignore
       console.error(yellow(`error: ${white(`${oldPackage.error}`)}`));
-    } else {
+    }
+
+    else {
       await createPackage({ ...oldPackage, ...newPAckage }, true);
     }
-  } else {
+  }
+
+  else {
     await createPackage(installPakages(input), true);
   }
-} else if (input[0] === flags.version) {
+}
+
+else if (input[0] === flags.version) {
   Version(VERSION.VERSION);
-} else if (input[0] === flags.help) {
+}
+
+else if (input[0] === flags.help) {
   LogHelp(helpsInfo);
-} else if (input[0] === flags.custom) {
+}
+
+else if (input[0] === flags.custom) {
   const data = input[1].includes("=")
     ? input[1].split("=")
     : ["Error", "Add a valid package"];
-  const custom = {};
-  //@ts-ignore
+
+  const custom = {} as { [key: string]: string};
+
   custom[data[0]] = data[1];
   // * if import_map exists update it
   if (existsSync("./import_map.json")) {
@@ -49,11 +53,15 @@ if (input[0] === keyWords.install || input[0] === keyWords.i) {
     const oldPackage = updatePackages(data);
 
     await createPackage({ ...custom, ...oldPackage }, true);
-  } else {
+  }
+
+  else {
     // * else create package
     await createPackage(custom, true);
   }
-} else if (input[0] === keyWords.uninstall) {
+}
+
+else if (input[0] === keyWords.uninstall) {
   const pkg: string = input[1].trim();
 
   if (existsSync("./import_map.json")) {
@@ -78,7 +86,9 @@ if (input[0] === keyWords.install || input[0] === keyWords.i) {
 
     console.error(error);
   }
-} else if (input[0] === keyWords.tool) {
+}
+
+else if (input[0] === keyWords.tool) {
   const tool = input[1].trim();
   if (Object.keys(dbTool).includes(tool)) {
     console.log(
@@ -97,9 +107,13 @@ if (input[0] === keyWords.install || input[0] === keyWords.i) {
       " is not in the tools database"
     );
   }
-} else if (input[0] === keyWords.update) {
+}
+
+else if (input[0] === keyWords.update) {
   updateTrex();
-} else if (input[0] === flags.deps) {
+}
+
+else if (input[0] === flags.deps) {
   const process = Deno.run({
     cmd: [
       "deno",
@@ -118,6 +132,8 @@ if (input[0] === keyWords.install || input[0] === keyWords.i) {
 
   const out = await process.output();
   console.log(decoder.decode(out));
-} else {
+}
+
+else {
   LogHelp(helpsInfo);
 }
