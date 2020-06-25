a3f1feffa1962eae2a2eb1fbc8c3b22277df796c
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-06-25 17:01:19 -0400

    offline message

--- tools/database.ts
+++ tools/database.ts
@@ -1,6 +1,8 @@
+import { offLine } from "../utils/logs.ts"
+
 const response = await fetch(
   "https://raw.githubusercontent.com/crewdevio/Trex/master/database.json"
-);
+).catch( (_) => offLine()) as Response;
 // * get data from database.json
 
 const data = await response.json();
--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,9 @@
+import { offLine } from './logs.ts';
+
+// * get all thirt party
 const response = await fetch(
   "https://raw.githubusercontent.com/denoland/deno_website2/master/database.json"
-); // * get all thirt party
+  ).catch( (_) => offLine()) as Response;
 
 const database = await response.json();
 
