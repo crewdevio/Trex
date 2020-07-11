18cca72fb478d10a93c35a676397adc0d8e8bb46
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-07-11 14:29:45 -0400

    fixed: error message when install no valid package

--- Trex.ts
+++ Trex.ts
@@ -35,7 +35,7 @@ async function mainCli() {
       }
 
       catch (_) {
-        throw new Error(red("the import_map.json file don't have a valid format")).message;
+        throw new Error(_).message;
       }
     }
 
