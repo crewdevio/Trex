77eb6fcb470b4e82c9f2fdbca04f7a94c9074c53
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-09-24 21:35:08 -0400

    chore: convert delete command in a function

---
+++ handlers/delete_package.ts
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Crew Dev.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { createPackage, getImportMap } from "./handle_files.ts";
+import { haveVersion } from "./handle_delete_package.ts";
+import { exist_imports } from "./handle_packages.ts";
+import { colors } from "../imports/fmt.ts";
+import { exists } from "../imports/fs.ts";
+import { STD } from "../utils/info.ts";
+
+const { green, yellow, red } = colors;
+
+export async function deletepackage(toDelete: string) {
+  if (await exists("./import_map.json")) {
+    try {
+      const pkg: string = toDelete.trim();
+      const Packages = JSON.parse(await getImportMap());
+
+      if (Packages?.imports) {
+        delete Packages.imports[
+          STD.includes(haveVersion(pkg))
+            ? haveVersion(pkg) + "/"
+            : haveVersion(pkg)
+        ];
+
+        const newPackage = exist_imports(Packages);
+
+        await createPackage(newPackage);
+
+        console.log(yellow(pkg + ":"), green(" removed from import_map.json"));
+      } else {
+        throw new Error(red("'imports' key not found in import_map.json"))
+          .message;
+      }
+    } catch (_) {
+      throw new Error(
+        red(
+          _ instanceof TypeError
+            ? "add the name of the package to remove"
+            : "the import_map.json file does not have a valid format."
+        )
+      ).message;
+    }
+  } else {
+    console.error(red("import_map.json"));
+    return;
+  }
+}
