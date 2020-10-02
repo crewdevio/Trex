03ca4f78761036db57182925198ff11d3ae3f15a
Erick Sosa <ericksosagarcias@gmail.com>
2020-10-02 16:45:36 -0400

    fix: diff name change

--- proxy/files/testing.ts
+++ proxy/files/testing.ts
@@ -8,6 +8,6 @@
 
 import * as asserts from "https://deno.land/std/testing/asserts.ts";
 import * as bench from "https://deno.land/std/testing/bench.ts";
-import * as diff from "https://deno.land/std/testing/diff.ts";
+import * as diff from "https://deno.land/std/testing/_diff.ts";
 
 export { asserts, bench, diff };
