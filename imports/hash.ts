ae039610b1bc8710c4154131acfe24514505061b
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-11-28 00:13:52 -0400

    chore: update to std@0.79.0

--- imports/hash.ts
+++ imports/hash.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.76.0/hash/mod.ts";
+export * from "https://deno.land/std@0.79.0/hash/mod.ts";
--- imports/path.ts
+++ imports/path.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.76.0/path/mod.ts";
+export * from "https://deno.land/std@0.79.0/path/mod.ts";
