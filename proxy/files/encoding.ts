1e1c1b4ee40e25504f88ad9570baba2f803a7c5f
Erick Sosa <ericksosagarcias@gmail.com>
2020-12-05 18:21:43 -0400

    fix: typo error

--- proxy/files/encoding.ts
+++ proxy/files/encoding.ts
@@ -13,7 +13,7 @@ import * as base64 from "https://deno.land/std/encoding/base64.ts";
 import * as binary from "https://deno.land/std/encoding/binary.ts";
 import * as toml from "https://deno.land/std/encoding/toml.ts";
 import * as utf8 from "https://deno.land/std/encoding/utf8.ts";
-import * as yalm from "https://deno.land/std/encoding/yaml.ts";
+import * as yaml from "https://deno.land/std/encoding/yaml.ts";
 import * as csv from "https://deno.land/std/encoding/csv.ts";
 import * as hex from "https://deno.land/std/encoding/hex.ts";
 
