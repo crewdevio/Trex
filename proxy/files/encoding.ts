2de1818683484d608919f8091ff42786865b45d4
Erick Sosa <ericksosagarcias@gmail.com>
2021-02-22 17:21:42 -0400

    Update encoding.ts

--- proxy/files/encoding.ts
+++ proxy/files/encoding.ts
@@ -12,9 +12,8 @@ import * as base32 from "https://deno.land/std/encoding/base32.ts";
 import * as base64 from "https://deno.land/std/encoding/base64.ts";
 import * as binary from "https://deno.land/std/encoding/binary.ts";
 import * as toml from "https://deno.land/std/encoding/toml.ts";
-import * as utf8 from "https://deno.land/std/encoding/utf8.ts";
 import * as yaml from "https://deno.land/std/encoding/yaml.ts";
 import * as csv from "https://deno.land/std/encoding/csv.ts";
 import * as hex from "https://deno.land/std/encoding/hex.ts";
 
-export { base32, base64, base64url, binary, csv, hex, toml, yaml, utf8, ascii86 };
+export { base32, base64, base64url, binary, csv, hex, toml, yaml, ascii86 };
